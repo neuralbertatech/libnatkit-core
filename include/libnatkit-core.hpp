e8ecf89990080706d89526d9d01be831cb06a6b5
Zach Selk <zrselk@gmail.com>
2024-04-17 11:38:55 -0600

    Build Fix

--- include/libnatkit-core.hpp
+++ include/libnatkit-core.hpp
@@ -17,7 +17,7 @@
 namespace nat {
 namespace core {
 
-inline template<typename T, typename... Args>
+template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
 {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
