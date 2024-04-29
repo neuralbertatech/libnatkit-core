64f801acabf1783afbda0aaddce6862958ed8d68
klbauer1 <146482137+klbauer1@users.noreply.github.com>
2024-04-29 15:00:29 -0600

    Update BasicTopicInformation.c

--- src/BasicTopicInformation.c
+++ src/BasicTopicInformation.c
@@ -2,41 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-BasicTopicInformation createBasicTopicInformation(const char *kafkaTopicString) {
-    const auto splitName = split(kafkaTopicString, '-');
+BasicTopicInformation newBasicTopicInformation(const char *kafkaTopicString) {
+    const auto *splitName = strtok(kafkaTopicString, '-');
     if(len(splitName) != 4) {
         printf("Topic String does not contain the four parts\n");
-        return 0;
+        return;
     }
     const auto streamTypeName = splitName[0];
     const auto streamIdString = splitName[1];
     const auto streamEncoderName = splitName[2];
     const auto streamSchemaName = splitName[3];
 
-    const auto streamTypeMaybe = streamTypeFromString(streamTypeName);
+    auto streamTypeMaybe = streamTypeFromString(streamTypeName);
     const auto serializationTypeMaybe = getSerializationTypeFromString(streamEncoderName);
     const auto streamId = strtoll(streamIdString, NULL, 10);  //base 10
 
     if (streamTypeMaybe == NULL) {
         printf("\"%s\" is not a valid stream type\n", streamTypeName);
-        return 0;
+        return;
     }
     if (serializationTypeMaybe == NULL) {
         printf("\"%s\" is not a valid serialization type\n");
-        return 0;
+        return;
     }
 
-    return BasicTopicInformation(streamTypeMaybe, serializationTypeMaybe, streamId, streamSchemaName);
+    createBasicTopicInformation(streamTypeMaybe, serializationTypeMaybe, streamId, streamSchemaName);
 }
 
-BasicTopicInformation toString() {
-    BasicTopicInformation buffer[256];
-    sprintf(buffer, "BasicTopicInformation: {type=\"%s\", id=\"%s\", serializationType=\"%s\", schemaName=\"%s\"}", type, id, serializationType, schemaName);
-    return buffer
+char* basicTopicInformationToString(const BasicTopicInformation *topicInfo) {
+    char buffer[256];
+    snprintf(buffer, sizeof(buffer), "BasicTopicInformation: {type=\"%s\", id=\"%s\", serializationType=\"%s\", schemaName=\"%s\"}", streamTypeToString(topicInfo->type), topicInfo->id, serializationTypeToString(topicInfo->serializationType), topicInfo->schemaName);
+    return strdup(buffer);
 }
 
-BasicTopicInformaiton toTopicString() {
-    BasicTopicInformation buffer[256];
-    sprintf(buffer, "\"%s\"-\"%s\"-\"%s\"-\"%s\"}", type, id, serializationType, schemaName);
-    return buffer;
+char* basicTopicInformationToTopicString(const BasicTopicInformation *topicInfo) {
+    char buffer[256];
+    snprintf(buffer, sizeof(buffer), "%s-%d-%s-%s", streamTypeToString(topicInfo->type), topicInfo->id, serializationTypeToString(topicInfo->serializationType), topicInfo->schemaName);
+    return strdup(buffer);
 }
