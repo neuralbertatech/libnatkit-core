51e11424fb936dfb89c55cd1323651fe6ab22e1c
klbauer1 <146482137+klbauer1@users.noreply.github.com>
2024-04-30 17:05:54 -0600

    Update StreamType.c

--- src/StreamType.c
+++ src/StreamType.c
@@ -3,32 +3,35 @@
 const char *toString(StreamType streamType) {
     switch(streamType) {
         case DATA:
-            return streamTypeToStringMapping[DATA];
+            return streamTypeToStringMapping[DATA].string;
         case META:
-            return streamTypeToStringMapping[META];
+            return streamTypeToStringMapping[META].string;
         case EXECUTION_COMMAND:
-            return streamTypeToStringMapping[EXECUTION_COMMAND];
+            return streamTypeToStringMapping[EXECUTION_COMMAND].string;
         case HARDWARE_STATUS:
-            return streamTypeToStringMapping[HARDWARE_STATUS];
+            return streamTypeToStringMapping[HARDWARE_STATUS].string;
         case HARDWARE_CONFIGURATION:
-            return streamTypeToStringMapping[HARDWARE_CONFIGURATION];
+            return streamTypeToStringMapping[HARDWARE_CONFIGURATION].string;
         case LOGGING_LOG:
-            return streamTypeToStringMapping[LOGGING_LOG];
+            return streamTypeToStringMapping[LOGGING_LOG].string;
          case LOGGING_HEARTBEAT:
-            return streamTypeToStringMapping[LOGGING_HEARTBEAT];
+            return streamTypeToStringMapping[LOGGING_HEARTBEAT].string;
         default:
             assert(0);
             return NULL;
     }
 }
 
-const StreamType *streamTypeFromString(const char *streamTypeString) {
+StreamType streamTypeFromString(const char* streamTypeString) {
     char lowercaseType[256];
-    toLowerCase(streamTypeString, lowercaseType);
+    strcpy(lowercaseType, streamTypeString);
+    toLowerCase(lowercaseType);
 
-    if(strstr(initializeStringToStreamTypeMapping, lowercaseType) != NULL) {
-        return &initializeStringToStreamTypeMapping[lowercaseType];
-    } else {
-        return NULL;
+    for (int i = 0; i < sizeof(streamTypeToStringMapping) / sizeof(streamTypeToStringMapping[0]); i++) {
+        if (strcmp(streamTypeToStringMapping[i].string, lowercaseType) == 0) {
+            return streamTypeToStringMapping[i].type;
+        } else {
+            return NULL;
+        }
     }
 }
