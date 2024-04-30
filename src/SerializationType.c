3cb2cd4dc40ccfd2e6b59915d64958e1e247eeed
klbauer1 <146482137+klbauer1@users.noreply.github.com>
2024-04-30 12:45:16 -0600

    Update SerializationType.c

--- src/SerializationType.c
+++ src/SerializationType.c
@@ -3,18 +3,18 @@
 #include <assert.h>
 #include <libnatkit-core.h>
 
-MAX_STR_LEN = 256
+MAX_STR_LEN = 256;
 enum SerializationType getSerializationType(const char* encoderName) {
     char lowercaseEncoderName[MAX_STR_LEN];
     size_t i = 0;
     while (encoderName[i] != '\0' && i < MAX_STR_LEN -1) {
         lowercaseEncoderName[i] = tolower(encoderName[i]);
-        i++:
+        i++;
     }
     lowercaseEncoderName[i] = '\0';
 
     if (strcmp(lowercaseEncoderName, "json") == 0) {
-        return Json
+        return Json;
     }
 
     printf("Fatal Error: Invalid serialization type '%s'\n", encoderName);
@@ -37,8 +37,8 @@ SerializationType serializationTypeFromString(const char *serializationTypeStrin
     strcpy(lowercaseType, serializationTypeString);
     toLowercase(lowercaseType);
     for (size_t i = 0; i < sizeof(lowercaseStringToSerializationTypeMapping) / sizeof(lowercaseStringToSerializationTypeMapping[0]); ++i) {
-        if (strcmp(lowercaseStringToSerializationTypeMapping[i].lowercaseType, lowercaseType) == 0) {
-            return lowercaseStringToSerializationTypeMapping[i].type;
+        if (strcmp(lowercaseStringToSerializationTypeMapping[i].key, lowercaseType) == 0) {
+            return lowercaseStringToSerializationTypeMapping[i].value;
         }
     }
     return -1;
